a7afa11887025f446e17e597a720c4ed6273e77f
Christopher L Bray <clbray123@googlemail.com>
2014-09-22 10:18:14 +0100

    wait till list containers are available

--- src/assets/js/background.js
+++ src/assets/js/background.js
@@ -14,40 +14,45 @@
 			}
 		}
 	}
+	
+	var tagSearchInput   = document.createElement('input'),
+	    ruleSearchInput  = document.createElement('input'),
+	    macroSearchInput = document.createElement('input');
+	
+	tagSearchInput.className   = 'id-sb-tag-search-input jfk-textinput';
+	ruleSearchInput.className  = 'id-sb-rule-search-input jfk-textinput';
+	macroSearchInput.className = 'id-sb-macro-search-input jfk-textinput';
 
-	setTimeout(function() {
+	tagSearchInput.placeholder   = 'search';
+	ruleSearchInput.placeholder  = 'search';
+	macroSearchInput.placeholder = 'search';
+	
+	function injectSearchFields() {
 		var tagListContainer   = document.querySelector('.ID-tm-tags-block'),
-			ruleListContainer  = document.querySelector('.ID-tm-conditions-block'),
-			macroListContainer = document.querySelector('.ID-tm-macros-block'),
-			tagListItems       = document.querySelectorAll('.ID-tm-tags-block li'),
-			ruleListItems      = document.querySelectorAll('.ID-tm-rules-block li'),
-			macroListItems     = document.querySelectorAll('.ID-tm-macros-block li'),
-			tagSearchInput     = document.createElement('input'),
-			ruleSearchInput    = document.createElement('input'),
-			macroSearchInput   = document.createElement('input');
-
-		tagSearchInput.className   = 'id-sb-tag-search-input jfk-textinput';
-		ruleSearchInput.className  = 'id-sb-rule-search-input jfk-textinput';
-		macroSearchInput.className = 'id-sb-macro-search-input jfk-textinput';
-
-		tagSearchInput.placeholder   = 'search';
-		ruleSearchInput.placeholder  = 'search';
-		macroSearchInput.placeholder = 'search';
-
-		tagSearchInput.addEventListener('keyup', function() {
-			filterItems(tagListItems, this.value);
-		});
-
-		ruleSearchInput.addEventListener('keyup', function() {
-			filterItems(ruleListItems, this.value);
-		});
-
-		macroSearchInput.addEventListener('keyup', function() {
-			filterItems(macroListItems, this.value);
-		});
-
-		tagListContainer.insertBefore(tagSearchInput, tagListContainer.querySelector('li'));
-		ruleListContainer.insertBefore(ruleSearchInput, ruleListContainer.querySelector('li'));
-		macroListContainer.insertBefore(macroSearchInput, macroListContainer.querySelector('li'));
-	}, 1000);
+		    ruleListContainer  = document.querySelector('.ID-tm-conditions-block'),
+		    macroListContainer = document.querySelector('.ID-tm-macros-block'),
+		    tagListItems       = document.querySelectorAll('.ID-tm-tags-block li'),
+		    ruleListItems      = document.querySelectorAll('.ID-tm-rules-block li'),
+		    macroListItems     = document.querySelectorAll('.ID-tm-macros-block li');
+		
+		if ( ! tagListContainer || ! ruleListContainer || ! macroListContainer) {
+			setTimeout(injectSearchFields, 2000);
+		} else {
+			tagSearchInput.addEventListener('keyup', function() {
+				filterItems(tagListItems, this.value);
+			});
+	
+			ruleSearchInput.addEventListener('keyup', function() {
+				filterItems(ruleListItems, this.value);
+			});
+	
+			macroSearchInput.addEventListener('keyup', function() {
+				filterItems(macroListItems, this.value);
+			});
+	
+			tagListContainer.insertBefore(tagSearchInput, tagListContainer.querySelector('li'));
+			ruleListContainer.insertBefore(ruleSearchInput, ruleListContainer.querySelector('li'));
+			macroListContainer.insertBefore(macroSearchInput, macroListContainer.querySelector('li'));
+		}
+	}
 }(window, document));
